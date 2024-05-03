02292e06080648ffcc62cf24a803bed6130dc084
rybakN <dnb4949@gmail.com>
2024-05-03 19:23:04 +0700

    del: delete castl module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,9 @@ import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { CaslModule } from './casl/casl.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, PostsModule, CaslModule],
+  imports: [UsersModule, AuthModule, PostsModule],
   controllers: [AppController],
   providers: [
     AppService,
