604b0efa3462dc14dd076d16108fc784efef2c50
rybakN <dnb4949@gmail.com>
2024-05-03 19:17:00 +0700

    feat: add authorization

---
+++ src/casl/casl-ability.factory/casl-ability.factory.ts
@@ -0,0 +1,43 @@
+import {
+  AbilityBuilder,
+  ExtractSubjectType,
+  InferSubjects,
+  MongoAbility,
+  createMongoAbility,
+} from '@casl/ability';
+import { Injectable } from '@nestjs/common';
+import { Post } from 'src/posts/entities/post.entity';
+import { User } from 'src/users/entities/user.entity';
+
+export enum Action {
+  Manage = 'manage',
+  Create = 'create',
+  Read = 'read',
+  Update = 'update',
+  Delete = 'delete',
+}
+
+type Subjects = InferSubjects<typeof Post | typeof User> | 'all';
+
+export type AppAbility = MongoAbility<[Action, Subjects]>;
+
+@Injectable()
+export class CaslAbilityFactory {
+  createForUser(user: User) {
+    const { can, cannot, build } = new AbilityBuilder(createMongoAbility);
+
+    if (user.role === 'ADMIN') {
+      can(Action.Manage, 'all'); // read-write access to everything
+    } else {
+      can(Action.Read, 'all'); // read-only access to everything
+    }
+
+    can(Action.Update, Post, { userId: user.id });
+
+    return build({
+      // Read https://casl.js.org/v6/en/guide/subject-type-detection#use-classes-as-subject-types for details
+      detectSubjectType: (item) =>
+        item.constructor as ExtractSubjectType<Subjects>,
+    });
+  }
+}
